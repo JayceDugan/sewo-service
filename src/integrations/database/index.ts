df93575ba7ca029070ce9711ecf603108da33c98
Jayce Dugan <jgtdsoftware@gmail.com>
2022-03-21 19:41:45 +1000

    feat: Initial commit.

--- src/integrations/database/index.ts
+++ src/integrations/database/index.ts
@@ -5,19 +5,24 @@ const logger = {
   info: (m: string) => console.log(m)
 }
 
+let connection: Knex | null = null;
+
 export const createSqlConnection = async (client?: string | typeof Knex.Client) => {
   try {
-    const databaseClient = knex(client ? { ...config, client } : config)
+    if (connection) return { connection }
+
+    const databaseClient: Knex = knex(client ? { ...config, client } : config)
     const connectionInfo = config.connection as Knex.PgConnectionConfig;
 
     // Test Connection
     databaseClient.raw('SELECT 1')
-      .then(() => logger.info(`DB Connection to ${connectionInfo.host}:${connectionInfo.port} established, db: ${connectionInfo.database}`))
+      .then(() => {
+        logger.info(`DB Connection to ${connectionInfo.host}:${connectionInfo.port} established, db: ${connectionInfo.database}`)
+        connection = databaseClient
+      })
       .catch((e: Error) => logger.info(`DB Connection failed: ${e}`))
 
-    return {
-      databaseClient
-    }
+    return { connection }
   } catch (err) {
     logger.info((err as Error).message)
   }
